2a12e2b220044630b441f959a24e9659af687ce0
Daniele Lacamera <root@danielinux.net>
2016-02-14 17:21:46 +0100

    Added patching of ct-ng

--- runme.sh
+++ runme.sh
@@ -4,6 +4,11 @@ set -x
 THIS_DIR="$(pwd | sed 's/\//\\\//g')"
 echo $THIS_DIR
 
+if (! test -f .ct-ng.patched); then
+    patch -p0 < ./ctng-custom-elf2flt.patch
+    touch .ct-ng.patched
+fi
+
 cat arm-frosted-eabi.config.in | sed -e "s/__CURRENT_DIR__/$THIS_DIR/g" > .config
 
 ct-ng build
