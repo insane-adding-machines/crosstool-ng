39fba2da25194e4c9f966a9f226b223b8d0a2473
Daniele Lacamera <dan@danielinux.net>
2017-09-17 22:49:07 +0200

    Added runme-j1.sh to build with one job

---
+++ runme-j1.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -x
+
+THIS_DIR="$(pwd | sed 's/\//\\\//g')"
+echo $THIS_DIR
+
+if (! test -f .ct-ng.patched); then
+    patch -p0 < ./ctng-custom-elf2flt.patch
+    touch .ct-ng.patched
+fi
+
+mkdir $PWD/cmd
+cd crosstool-ng
+autoreconf -i -f
+./configure --prefix=$PWD/../cmd && make && make install
+cd ..
+
+cat arm-frosted-eabi.config.in | sed -e "s/__CURRENT_DIR__/$THIS_DIR/g"| sed -e "s/PARALLEL_JOBS.*/PARALLEL_JOBS=1/g" > .config
+#cmd/bin/ct-ng menuconfig
+cmd/bin/ct-ng build $@
